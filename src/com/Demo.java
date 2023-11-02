bd55e894a452921050094ccf2a2e09e3632f9289
unknown <ravikhedkar108@gmail.com>
2023-11-02 18:08:22 +0530

    file added

---
+++ src/com/Demo.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Demo {
+public static void main(String[] args) {
+	for(int r=1;r<=5;r++) {
+		for(int c=1;c<=5;c++) {
+			System.out.print("*");
+		}
+		System.out.println(" ");
+	}
+	System.out.println();
+}
+}
